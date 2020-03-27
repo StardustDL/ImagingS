7f83559648a8af3b2feff0171ac528fd15b7bb47
StardustDL <stardustdl@163.com>
2020-03-27 21:15:32 +0800

    fix ui test error on linux

--- test/__init__.py
+++ test/__init__.py
@@ -1,8 +1,5 @@
 import sys
 import os
-import platform
 
 sys.path.append(
     (os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))))
-
-is_linux = True if platform.system() == "Linux" else False
--- test/ui/test_ui.py
+++ test/ui/test_ui.py
@@ -1,13 +1,11 @@
 from PyQt5.QtWidgets import QMainWindow
 from src.ui import MainWindow
-from . import is_linux
 
 
 def test_MainWindow(qtbot):
     window = QMainWindow()
     mui = MainWindow()
     mui.setupUi(window)
-    if not is_linux:
-        window.show()
+    window.show()
     # qtbot.addWidget(window)
     assert window.windowTitle() == "ImagingS"
