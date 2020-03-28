f19d3313a8b0ebd602679448d0aa4e2e6a502e4e
StardustDL <stardustdl@163.com>
2020-03-28 20:53:02 +0800

    add tools script

---
+++ tools.ps1
@@ -0,0 +1,62 @@
+if ($args.Count -gt 0) {
+    switch ($args[0]) {
+        "dep" {
+            Write-Output "Install dependencies..."
+            pip install -r requirements.txt
+        }
+        "dep-dev" {
+            Write-Output "Install dependencies for development..."
+            pip install pytest flake8
+            pip install pytest-qt pytest-cov
+            npm install -g pyright
+        }
+        "clean" {
+            Write-Output "Clean generated files.."
+            Get-ChildItem -Include .coverage | Remove-item
+            Get-ChildItem -Include htmlcov | Remove-item -Recurse
+            Get-ChildItem -Include .pytest_cache | Remove-item -Recurse
+            Get-ChildItem -Include __pycache__ -Recurse | Remove-item -Recurse
+            Get-ChildItem ./ImagingS/Gui/ui -Exclude .gitignore | Remove-item -Recurse
+        }
+        "clean-ui" {
+            Write-Output "Clean generated UI files.."
+            Get-ChildItem ./ImagingS/Gui/ui -Exclude .gitignore | Remove-item -Recurse
+        }
+        "gen-ui" {
+            Write-Output "Generate UI files..."
+            python -m ImagingS.Gui.uic
+        }
+        "gui" {
+            Write-Output "Run GUI..."
+            python -m ImagingS.Gui
+        }
+        "cli" {
+            Write-Output "Run CLI..."
+            python -m ImagingS.Cli
+        }
+        "lint" {
+            Write-Output "Lint..."
+            Write-Output "Flake8 check..."
+            # stop the build if there are Python syntax errors or undefined names
+            flake8 . --count --select=E9, F63, F7, F82 --exclude=.svn, CVS, .bzr, .hg, .git, __pycache__, .tox, .eggs, *.egg, _ui_*.py --show-source --statistics
+            # exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
+            flake8 . --count --exclude=.svn, CVS, .bzr, .hg, .git, __pycache__, .tox, .eggs, *.egg, _ui_*.py --exit-zero --max-complexity=10 --max-line-length=127 --statistics
+            Write-Output "Pyright check..."
+            pyright
+        }
+        "test" {
+            Write-Output "Test..."
+            pytest --cov=. --cov-report=term --cov-report=html
+        }
+        "test-noui" {
+            Write-Output "Test (without UI)..."
+            pytest --ignore test/ui --cov=. --cov-report=term --cov-report=html
+        }
+        Default {
+            Write-Output "Unrecognized command"
+        }
+    }
+}
+else {
+    Write-Output "Missing command"
+}
