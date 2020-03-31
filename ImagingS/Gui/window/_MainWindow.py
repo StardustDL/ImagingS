83d171c158e89aea334b572ffa4bba82c96c26b9
StardustDL <stardustdl@163.com>
2020-03-31 18:54:10 +0800

    add brush-create

--- ImagingS/Gui/window/_MainWindow.py
+++ ImagingS/Gui/window/_MainWindow.py
@@ -1,8 +1,9 @@
 import ImagingS.Gui.ui as ui
 from ImagingS.document import Document
 from ImagingS.Gui.app import Application
+from ImagingS.core import Color, brush
 
-from PyQt5.QtWidgets import QMainWindow, QFileDialog
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QColorDialog
 
 
 class MainWindow(QMainWindow, ui.MainWindow):
@@ -14,7 +15,10 @@ class MainWindow(QMainWindow, ui.MainWindow):
         self.actNew.triggered.connect(self.actNew_triggered)
         self.actSave.triggered.connect(self.actSave_triggered)
         self.actOpen.triggered.connect(self.actOpen_triggered)
-
+        self.actDrawings.triggered.connect(self.actDrawings_triggered)
+        self.actProperties.triggered.connect(self.actProperties_triggered)
+        self.actBrushes.triggered.connect(self.actBrushes_triggered)
+        self.actBrushSolid.triggered.connect(self.actBrushSolid_triggered)
         Application.current().documentChanged.connect(self.app_documentChanged)
         self.app_documentChanged()
 
@@ -22,6 +26,32 @@ class MainWindow(QMainWindow, ui.MainWindow):
         hasDoc = Application.current().document is not None
         self.actClose.setEnabled(hasDoc)
         self.gvwMain.setEnabled(hasDoc)
+        self.mnuDrawing.setEnabled(hasDoc)
+        self.mnuTransform.setEnabled(hasDoc)
+        self.mnuBrush.setEnabled(hasDoc)
+
+    def actBrushes_triggered(self):
+        if self.actBrushes.isChecked():
+            self.dwgBrushes.show()
+        else:
+            self.dwgBrushes.hide()
+
+    def actProperties_triggered(self):
+        if self.actProperties.isChecked():
+            self.dwgProperties.show()
+        else:
+            self.dwgProperties.hide()
+
+    def actDrawings_triggered(self):
+        if self.actDrawings.isChecked():
+            self.dwgDrawings.show()
+        else:
+            self.dwgDrawings.hide()
+
+    def actBrushSolid_triggered(self):
+        color = QColorDialog.getColor()
+        br = brush.Solid(Color(color.red(), color.green(), color.blue()))
+        Application.current().document.brushes.append(br)
 
     def actClose_triggered(self):
         Application.current().document = None
